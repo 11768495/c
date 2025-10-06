1a0827e876930cae6d99fcf53c1db8f8d08eedf9
melmunmal <melmunmal@alu.edu.gva.es>
2025-10-06 12:07:33 +0200

    Add files via upload

---
+++ pin1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main() {
+	int pin;
+	printf ("dis-me PIN: ");
+	scanf ("%d", &pin);
+	if (pin==4321) printf ("BRAvo !!!");
+	else printf ("nein nein >:), prova altra vegada");
+return;
+}
