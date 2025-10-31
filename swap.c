754a17361929dec966c1acd9955f2d2038eadfa6
melmunmal <melmunmal@alu.edu.gva.es>
2025-10-31 12:26:07 +0100

    Fix swap function and main definition

--- swap.c
+++ swap.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-void main(){
+int main() {
     int a, b, aux;
 
     printf("Intro a: ");
-    scanf ("%d", &a);
+    scanf("%d", &a);
     printf("Intro b: ");
-    scanf("&d", &b);
-    aux=a;
-    a=b;
-    b=aux;
-    printf("Intercanviat a i b es: %d | &d\n", a, b);
-    return;
+    scanf("%d", &b);
+
+    aux = a;
+    a = b;
+    b = aux;
+
+    printf("Intercanviat a i b es: %d | %d\n", a, b);
+    return 0;
 }
